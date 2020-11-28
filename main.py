e0cd1ef176698c18fc248e3ed83af24ebff2cd93
Tenundor <71014004+Tenundor@users.noreply.github.com>
2020-11-28 19:21:45 +0300

    Создал функцию для загрузки фотографий последнего полета

--- main.py
+++ main.py
@@ -13,6 +13,21 @@ def download_image(image_url, image_path):
     )
 
 
+def fetch_spacex_last_launch():
+    spacex_last_launch_url = 'https://api.spacexdata.com/v3/launches/latest'
+    spacex_response = requests.get(spacex_last_launch_url)
+    spacex_response.raise_for_status()
+    spacex_response_content = spacex_response.json()
+    flight_number = spacex_response_content["flight_number"]
+    spacex_images_links = spacex_response_content["links"]["flickr_images"]
+    if not spacex_images_links:
+        print("Couldn't find photos of the last launch.")
+    else:
+        for image_index, image_link in enumerate(spacex_images_links, start=1):
+            filename = "./images/spacex_{0:03d}_launch_{1:03d}.jpg".format(flight_number, image_index)
+            download_image(image_link, filename)
+
+
 def fetch_spacex_launch_by_number(flight_number):
     spacex_url = 'https://api.spacexdata.com/v3/launches/past'
     spacex_response = requests.get(
@@ -20,8 +35,11 @@ def fetch_spacex_launch_by_number(flight_number):
         params={"flight_number": str(flight_number)}
     )
     spacex_response.raise_for_status()
-    spacex_images_links = spacex_response.json()[0]["links"]["flickr_images"]
-    for image_index, image_link in enumerate(spacex_images_links, start=1):
-        filename = "./images/spacex_{0:03d}_launch_{1:03d}.jpg".format(flight_number, image_index)
-        download_image(image_link, filename)
-
+    spacex_response_content = spacex_response.json()[0]
+    spacex_images_links = spacex_response_content["links"]["flickr_images"]
+    if not spacex_images_links:
+        print("Couldn't find photos of the launch with flight number {}.".format(flight_number))
+    else:
+        for image_index, image_link in enumerate(spacex_images_links, start=1):
+            filename = "./images/spacex_{0:03d}_launch_{1:03d}.jpg".format(flight_number, image_index)
+            download_image(image_link, filename)

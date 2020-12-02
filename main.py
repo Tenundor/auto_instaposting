5b85ccabac0efd909d6a616d69ed4f762c503358
Antonio <Tenundor@gmail.com>
2020-12-02 13:38:32 +0300

    Внес небольшие правки в оформление кода

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ def download_file(file_url, file_path):
     Path(file_path.parent).mkdir(parents=True, exist_ok=True)
     response = requests.get(file_url, verify=False)
     response.raise_for_status()
-    file_path.write_bytes(
-        response.content
-    )
+    file_path.write_bytes(response.content)
 
 
 def fetch_spacex_last_launch():

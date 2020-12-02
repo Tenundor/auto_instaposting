4547f4ab446c503abcd664969ba1c86ffb44ed5c
Antonio <Tenundor@gmail.com>
2020-12-02 13:36:42 +0300

    Убрал функцию загрузки фотографий spacex по id

--- main.py
+++ main.py
@@ -24,26 +24,6 @@ def fetch_spacex_last_launch():
         download_file(spacex_image_url, filename)
 
 
-def fetch_spacex_launch_by_number(flight_number):
-    spacex_url = 'https://api.spacexdata.com/v3/launches/past'
-    spacex_response = requests.get(
-        spacex_url,
-        params={"flight_number": str(flight_number)}
-    )
-    spacex_response.raise_for_status()
-    try:
-        spacex_response_content = spacex_response.json()[0]
-        spacex_images_links = spacex_response_content["links"]["flickr_images"]
-        if not spacex_images_links:
-            print("Couldn't find photos of the launch with flight number {}.".format(flight_number))
-        else:
-            for image_index, image_link in enumerate(spacex_images_links, start=1):
-                filename = "./images/spacex_{0:03d}_launch_{1:03d}.jpg".format(flight_number, image_index)
-                download_file(image_link, filename)
-    except IndexError:
-        print("Flight number {} not found".format(flight_number))
-
-
 def get_file_extension_from_url(url):
     extension_index = url.rindex(".")
     return url[extension_index + 1:]
